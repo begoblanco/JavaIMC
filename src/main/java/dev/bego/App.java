beb287123922559935a4824780791717db2e6c36
Bego Blanco <begoblanfer@gmail.com>
2024-07-02 12:47:52 +0200

    class app and class imc calculator

---
+++ src/main/java/dev/bego/App.java
@@ -0,0 +1,35 @@
+package dev.bego;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Your weight in Kg: "); //using comma
+        double weight = scanner.nextDouble();
+
+        System.out.print("Your height in meters: "); //using comma
+        double height = scanner.nextDouble();
+
+        // double peso = 46; 
+        // double estatura = 1,58; 
+        
+        CalculatorIMC calculator = new CalculatorIMC();
+        
+    
+        double imc = calculator.calculateIMC(weight, height);
+        String result = calculator.tableIMC(imc);
+
+        System.out.println("IMC: " + imc);
+        System.out.println();      
+        System.out.println("Result: " + result);
+
+        scanner.close();
+    }
+}
---
+++ src/main/java/dev/bego/CalculatorIMC.java
@@ -0,0 +1,30 @@
+package dev.bego;
+
+public class CalculatorIMC {
+
+    public double calculateIMC(double weight, double height){
+        double imc = weight / Math.pow(height, 2);
+        return imc;
+    }
+    
+
+    public String tableIMC(double imc) {
+        if (imc < 16) {
+            return "Delgadez severa";
+        } else if (imc >= 16 && imc < 17) {
+            return "Delgadez moderada";
+        } else if (imc >= 17 && imc < 18.5) {
+            return "Delgadez leve";
+        } else if (imc >= 18.5 && imc < 25) {
+            return "Peso normal";
+        } else if (imc >= 25 && imc < 30) {
+            return "Sobrepeso";
+        } else if (imc >= 30 && imc < 35) {
+            return "Obesidad leve";
+        } else if (imc >= 35 && imc < 40) {
+            return "Obesidad moderada";
+        } else {
+            return "Obesidad mórbida";
+        }
+    }
+}
